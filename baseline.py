3ca49d66e0ad1ea2f546f6a02d64abaea8bdd7a9
Andrew Mauboussin <ammaub@college.harvard.edu>
2015-11-24 22:28:33 -0500

    added parameters to baseline

--- baseline.py
+++ baseline.py
@@ -13,14 +13,11 @@ def main():
 
 
 
-def fit_baseline(X):
+def fit_baseline(X, n_topics=20, n_iter=1500):
     """Fits an LDA model on feature matrix X and prints how long it took"""
-
-
-    model = lda.LDA(n_topics=20, n_iter=1500, random_state=1)
+    model = lda.LDA(n_topics=n_topics, n_iter=n_iter, random_state=1)
     with Timer() as t:
-        model.fit(X)  # model.fit_transform(X) is also available
-
+        model.fit(X)
     print 'Fit %d articles with a vocab of %d words in %.3f seconds' % (X.shape + (t.interval, ))
     return model
 
